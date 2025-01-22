fecc5f19e253d47578da97065956f9e6526118c8
kushagra verma <kushagraverma123lko@gmail.com>
2025-01-22 03:27:19 +0300

    Initial Commit

---
+++ first Project/first_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_ProjectApp.swift
+//  first Project
+//
+//  Created by Kushagra Verma on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct first_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ first ProjectTests/first_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  first_ProjectTests.swift
+//  first ProjectTests
+//
+//  Created by Kushagra Verma on 22/01/2025.
+//
+
+import Testing
+@testable import first_Project
+
+struct first_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
